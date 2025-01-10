451e9fb8e5f026637f07f767cd614a1eed12391c
Debosmit Neogi <debosmit@buffalo.edu>
2025-01-10 14:38:57 -0500

    Added code and test

--- test_app.py
+++ test_app.py
@@ -1,3 +1,11 @@
 '''
 File for unit testing 
 '''
+
+from app import app
+
+def test_home():
+    response = app.test_client().get("/")
+
+    assert response.status_code == 200 ## success
+    assert response.data ==b"Hello world"
